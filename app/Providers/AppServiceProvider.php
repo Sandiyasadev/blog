ee0b508d1b3888689708d3df6e145daddfd381d3
Sandiyasadev <codepansya@gmail.com>
2026-01-08 18:42:23 +0800

    ficxx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Post;
 use App\Observers\PostObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Post::observe(PostObserver::class);
     }
 }
